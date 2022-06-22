5361b7073af3c915e5037a0380489be1f5392544
monishajayasankar <monijaya33@gmail.com>
2022-06-21 23:12:32 -0400

    Add project files.

---
+++ WebOrderitem/Controllers/OrderController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebOrderitem.Models;
+
+namespace WebOrderitem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetCartBy(int id)
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("http://localhost:44363");
+                var responseTask = client.GetAsync("MenuItem");
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+
+
+
+
+                if (result.IsSuccessStatusCode)
+                {
+                    List<Cart> Items = new List<Cart>();
+
+                    string jsonData = result.Content.ReadAsStringAsync().Result;
+
+
+                    Items = JsonConvert.DeserializeObject<List<Cart>>(jsonData);
+                    Cart obj = Items.SingleOrDefault(item => item.Id == id);
+
+                    obj.UserId = 1;
+
+
+
+
+
+                    return Ok(obj);
+
+                }
+                else
+                {
+
+                    return BadRequest();
+
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
---
+++ WebOrderitem/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebOrderitem.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        //  public int MenuItemId { get; set; }
+        public int UserId { get; set; }
+        // public int Quantity { get; set; }
+        public int price { get; set; }
+
+
+        public string Name { get; set; }
+    }
+}
